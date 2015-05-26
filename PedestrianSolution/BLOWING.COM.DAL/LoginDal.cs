01e602397f95aa529d1e7eecc309519f8271310c
unknown <sorcery.zzz@gmail.com>
2015-05-26 15:26:59 +0800

    Revert "Revert "create a master.""
    This reverts commit bcad5e0ef422ce12e3779a942795b840f1418e03.

---
+++ PedestrianSolution/BLOWING.COM.DAL/LoginDal.cs
@@ -0,0 +1,18 @@
+﻿using BLOWING.COM.IDAL;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BLOWING.COM.DAL
+{
+    public class LoginDal : ILoginDal
+    {
+
+        public string Login(string user, string passwd)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
